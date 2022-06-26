c669b548b0688f371ed2a4d201320a0a078790b4
Nardos221 <nardosayele9@gmail.com>
2022-06-26 04:05:04 -0700

    initalizing

---
+++ project 1/project 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    public class Exercise1
+    {
+        public static void Main()
+        {
+            int num1, num2;
+            Console.Write("\n\n");
+            Console.Write("Check whether two integers are equal or not:\n");
+            Console.Write("-------------------------------------------");
+            Console.Write("\n\n");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Input 1st number: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input 2nd number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                if (num1 == num2)
+                    Console.WriteLine(" they are equal");
+                else
+                    Console.WriteLine("are not equal.\n", num1, num2);
+            }
+        }
+    }
+}
